4b29bd04e1adf65fa12164c18b2c5c57c202e7e2
mhndra-f <hendrama.prvt@gmail.com>
2026-03-08 11:43:05 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,33 +15,34 @@ def main():
             break
 
         # Check if the user input is the "echo" command
-        if command.startswith("echo"):
+        elif command.startswith("echo"):
             print(command[5:])
 
         # Check if the user input is the "type" command
         elif command.startswith("type"):
             command = command[5:]
 
-            #Initialize path
-            path = os.environ.get("PATH", "")
-            path_dirs = path.split(os.pathsep)
-
-            # Check if the command_name is a shell builtin
+            # Check if the argument is a shell builtin
             if command in ["exit", "echo", "type"]:
                 print(f"{command} is a shell builtin")
-            
-            # Check if the command_name exsists in the PATH
+
+            # Check if the argument exists in the PATH
             else:
+                path = os.environ.get("PATH", "")
+                path_dirs = path.split(os.pathsep)
+                found = False
+
                 for directory in path_dirs:
                     path = os.path.join(directory, command)
-
-                    if os.path.exists(path) and os.access(path, os.X_OK):
-                        full_path = path
-                        print(f"{command} is {full_path}")
                 
-                    # If the command_name is not found
-                    else:
-                        print(f"{command}: not found")
+                    if os.path.exists(path) and os.access(path, os.X_OK):
+                        print(f"{command} is {full_path}")
+                        found = True
+                        break
+
+                # If the command_name is not found
+                if not found:
+                    print(f"{command}: not found")
 
         # If the user_input is not found
         else:
