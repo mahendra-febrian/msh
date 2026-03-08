27ef41dcfca16790d4f7a493102ff941e1b8b0f4
mhndra-f <hendrama.prvt@gmail.com>
2026-03-08 10:04:55 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ def main():
 
         # Wait for user input
         command = input()
+        
+        if command == "exit":
+            break
         print(f"{command}: command not found")
 
 
