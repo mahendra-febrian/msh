bf13c46ed897275d1c548eb65073b16f495bc30c
mhndra-f <hendrama.prvt@gmail.com>
2026-03-08 14:13:48 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -66,9 +66,6 @@ def main():
 
         # Check if the argument refers to an existing executable in the PATH
         else:
-            command = command.split(" ")
-            command = command[0]
-
             full_path = find_in_PATH(command)
 
             if full_path:
