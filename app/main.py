4af24da2b8d12ed36d95e28b6bc7563bcbf4ba2d
mhndra-f <hendrama.prvt@gmail.com>
2026-03-08 10:55:03 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -9,16 +10,26 @@ def main():
         # Wait for user input
         command = input()
 
+        # "exit" command
         if command == "exit":
             break
 
+        # "echo" command
         if command.startswith("echo"):
             print(command[5:])
+
+        # "type" command
         elif command.startswith("type"):
             if command[5:] in ["exit", "echo", "type"]:
                 print(f"{command[5:]} is a shell builtin")
+            elif :
+                for
+                if os.path.exists(os.path.split(path, os.pathsep)[i]) and os.access(PATH, os.X_OK):
+                    print(f"{command[5:]} is {PATH}")
             else:
                 print(f"{command[5:]}: not found")
+
+        # Command not found
         else:
             print(f"{command}: command not found")
 
