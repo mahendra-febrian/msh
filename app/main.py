c10cebaf2ef8164c8c6ad0066f8be6f341984926
mhndra-f <hendrama.prvt@gmail.com>
2026-03-08 14:00:02 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,33 +2,41 @@ import sys
 import os
 import subprocess
 
-def search_in_PATH(command, path_dirs):
-    found = False
-
-    for directory in path_dirs:
-        path = os.path.join(directory, command)
+def find_in_PATH(command):
+    for path in os.environ.get("PATH", "").split(os.pathsep):
+        full_path = os.path.join(directory, command)
                 
         if os.path.exists(path) and os.access(path, os.X_OK):
-            full_path = path
-            found = True
             return full_path
-            break
 
     # If the command_name is not found
-    if not found:
-        return False
+    return None
 
 def main():
     while True:
-        #Initialize path
-        path = os.environ.get("PATH", "")
-        path_dirs = path.split(os.pathsep)
-
         # TODO: Uncomment the code below to pass the first stage
         sys.stdout.write("$ ")
 
         # Wait for user input
         command = input()
+
+        # Parse single quotes
+        tokens = []
+        current = ""
+        is_in_single_quotes = False
+
+        for character in command:
+            if character == "'":
+                is_in_single_quotes = not is_in_single_quotes
+            elif char == " " and not is_in_single_quotes:
+                if current:
+                    tokens.append(current)
+                    current = ""
+            else:
+                current += char
+
+        if current:
+            tokens.append(current)
 
         # Check if the user input refers to the "exit" command
         if command == "exit":
@@ -64,12 +72,12 @@ def main():
 
             # Check if the argument refers to an existing executable in the PATH
             else:
-                result = search_in_PATH(arguments, path_dirs)
+                full_path = find_in_PATH(arguments)
 
-                if not result:
-                    print(f"{arguments}: not found")
-                else:
+                if full_path:
                     print(f"{arguments} is {result}")
+                else:
+                    print(f"{arguments}: not found")
 
         # Check if the argument refers to an existing executable in the PATH
         else:
@@ -77,12 +85,12 @@ def main():
             args = command[1:]
             command = command[0]
 
-            result = search_in_PATH(command, path_dirs)
+            full_path = find_in_PATH(command)
 
-            if not result:
-                print(f"{command}: command not found")
-            else:
+            if full_path:
                 subprocess.call([command] + args)
+            else:
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
